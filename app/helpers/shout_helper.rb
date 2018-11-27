c8906dd243add8f38122336bb1742c93e0393781
clair13 <theclairvoyant1313@yahoo.gr>
2018-11-27 18:54:02 +0200

    Refactor shout forms

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -1,4 +1,12 @@
 module ShoutHelper
+  def shout_form_for(content_type)
+    form_for Shout.new do |f|
+      f.hidden_field(:content_type, value: content_type) +
+      f.fields_for(:content) { |content_form| yield(content_form) } +
+      f.submit("Shout!")
+    end
+  end
+
   def like_button(shout)
     if current_user.liked?(shout)
       link_to "Unlike", unlike_shout_path(shout), method: :delete
